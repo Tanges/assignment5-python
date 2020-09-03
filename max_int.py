99322b0b526b6bbde47dc5449a3442fbad67c356
Alexander Þór <sorry112@gmail.com>
2020-09-03 09:55:34 +0000

    Algorithm improved for max_int and comments added

--- max_int.py
+++ max_int.py
@@ -1,3 +1,13 @@
+
 num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
+max_int = 0
+#Algorithm to find the highest integer by user input
+#We check if user input is higher than the user input before and replace
+#that number if it is, but leave it as is if it isn't
+while num_int > 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int > max_int:
+        max_int = num_int
+    else: continue
+
 print("The maximum is", max_int)    # Do not change this line
